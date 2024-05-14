a897d80c154d28b244a19e5735329a0978d6e1b3
Mykyta Martsevyi <53223543+nikmar13@users.noreply.github.com>
2024-05-14 22:05:07 +0300

    Update run_mpi_task.sh

--- run_mpi_task.sh
+++ run_mpi_task.sh
@@ -1,31 +1,21 @@
-#!/bin/bash
-
-# Початкова статистика
-echo -n > statistics.log
-
-# Створення файлу stagein_statistics для початкової задачі
-cp statistics.log stagein_statistics
-
-for i in {1..100}; do
-    # Оновлення файлу stagein_statistics перед кожним завданням
-    cp statistics.log stagein_statistics
-
-    # Подання завдання у чергу PBS
-    job_id=$(qsub mpi_task.pbs)
-
-    # Очікування завершення завдання (перевірка кожні 5 секунд)
-    while true; do
-        if qstat | grep -q "$job_id"; then
-            sleep 5
-        else
-            break
-        fi
-    done
-
-    # Оновлення файлу statistics.log після завершення завдання
-    cp stagein_statistics statistics.log
-done
-
-# Підсумковий вивід статистики
-echo "Final node process statistics:"
+#!/bin/bash                                                                                                                                                                                                      
+                                                                                                                                                                                                                 
+echo -n > statistics.log                                                                                                                                                                                         
+                                                                                                                                                                                                                 
+for i in {1..2}; do                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                                                                                              
+    job_id=$(qsub mpi_task.pbs)                                                                                                                                                                                  
+    echo "run $job_id"                                                                                                                                                                                           
+                                                                                                                                               
+    while true; do                                                                                                                                                                                               
+        if qstat | grep -q "$job_id"; then                                                                                                                                                                       
+            sleep 5                                                                                                                                                                                              
+        else                                                                                                                                                                                                     
+            break                                                                                                                                                                                                
+        fi                                                                                                                                                                                                       
+    done                                                                                                                                                                                                         
+    echo "finished checking"                                                                                                                                                                                                                                                                                                                                   
+done                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                                                                                                                
+echo "Final node process statistics:"                                                                                                                                                                            
 cat statistics.log
